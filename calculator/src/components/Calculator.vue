e9db33275598e106dde55dfd2e8e478573e9de87
Arnol Cáceres <116393375+arnolcb@users.noreply.github.com>
2023-07-16 00:55:54 -0500

    feat: create calculator component

---
+++ calculator/src/components/Calculator.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="font-mono flex p-6 bg-white text-center justify-center">
+        <div class="text-2xl font-bold">Calculator</div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
